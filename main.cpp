7b080383393e61a1e51a7b6f0dc967215de7ce05
kl456123 <kl456123@outlook.com>
2015-12-24 19:23:20 +0800

    成功实现加密为二进制流。传输时需传输table和code

--- main.cpp
+++ main.cpp
@@ -16,25 +16,25 @@ typedef struct Node {
     struct Node *parent;
     int flag;
     char c;
-    double freq;
-    Node(int freq=0,char cao='\0',Node* left=NULL,Node* right=NULL,Node* parent=NULL) {
-        left=left;
-        right=right;
-        parent=parent;
-        freq=freq;
-        c=cao;
+    int freq;
+    Node(int afreq=0,char ac='\0',Node* aleft=NULL,Node* aright=NULL,Node* aparent=NULL) {
+        left=aleft;
+        right=aright;
+        parent=aparent;
+        freq=afreq;
+        c=ac;
     }
 } Node,*pNode;
-struct cmp{
-    bool operator()(pNode node1, pNode node2){
-        return node1->freq < node2->freq;
+struct cmp {
+    bool operator()(pNode node1, pNode node2) {
+        return node1->freq > node2->freq;
     }
 };
 
-typedef struct Ans{
+typedef struct Ans {
     char c;
     string code;
-}Ans;
+} Ans;
 priority_queue<pNode,vector<pNode>,cmp > q;
 vector<Ans> ans;
 pNode Huffman(int n) {
@@ -56,55 +56,80 @@ pNode Huffman(int n) {
     }
     return q.top();
 }
-void Print(pNode root,string str){
-    if(root==NULL){
+void Print(pNode root,string str) {
+    if(root==NULL) {
         return ;
     }
-    if(root->left){
-            str+='0';
+    if(root->left) {
+        str+='0';
         Print(root->left,str);
     }
-    if(root->left==NULL&&root->right==NULL){
+    if(root->left==NULL&&root->right==NULL) {
         cout<<root->c<<"‘s code: "<<str<<endl;
-Ans one;
-one.c=root->c;
-one.code=str;
-ans.push_back(one);
+        Ans one;
+        one.c=root->c;
+        one.code=str;
+        ans.push_back(one);
     }
     str.erase(str.end()-1);
-    if(root->right){
+    if(root->right) {
         str+='1';
         Print(root->right,str);
     }
 
 }
+int charFreq[300];
 int main() {
     freopen("in.txt","r",stdin);
     string str;
-    int n;
-    char temp;
-    scanf("%d",&n);
-    scanf("%c",&temp);
-    for(int i=0; i<n; i++) {
+    string article;
+    char art;
+    int n=0;
+    while(~scanf("%c",&art)) {
+        article+=art;
+        if(charFreq[art]==0)
+            n++;
+        charFreq[art]++;
+    }
+    for(int i=0; i<300; i++) {
         int freq;
         char s[10];
-
-        scanf("%s %d",s,&freq);
+        if(charFreq[i]==0)continue;
+        freq=charFreq[i];
+        s[0]=i;
         pNode now=new Node(freq,s[0]);
-
+//printf("freq:%d\n",now->freq);
         q.push(now);
     }
     pNode root=Huffman(n);
     Print(root,str);
-   fstream ofile("out.txt");
-    for(int i=0;i<n;i++){
-            string c;
-    c+=ans[i].c;
-    c+="'s code:";
-    c+=ans[i].code;
-    c+='\n';
-    ofile<<c;
+    fstream ofile("out.txt");
+    for(int i=0; i<n; i++) {
+        string c;
+        c+=ans[i].c;
+        c+="'s code:";
+        c+=ans[i].code;
+        c+='\n';
+        ofile<<c;
     }
     ofile.close();
+    fstream code("code.txt");
+    for(int i=0; i<article.length(); i++) {
+        for(int j=0; j<ans.size(); j++) {
+            if(ans[j].c==article[i]) {
+                code<<ans[j].code;
+            }
+        }
+    }
+    code.close();
+    fstream table("table.txt");
+    for(int i=0; i<n; i++) {
+        string c;
+        c+=ans[i].c;
+        c+=ans[i].code;
+        c+=' ';
+        table<<c;
+    }
+    table.close();
     return 0;
 }
